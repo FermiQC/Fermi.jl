1e6d49612925cba11577e5c1822902ed75f4fbc9
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2020-09-03 12:14:56 -0400

    Fixed tests for CC after changing @energy

--- test/test_CI.jl
+++ test/test_CI.jl
@@ -1,8 +1,8 @@
 @reset
 @testset "CI" begin
-    wf = @energy ci
+    wf = @energy casci
     @test isapprox(wf.energy,-75.01775104333774,rtol=tol)
     @set ci_alg sparse
-    wf = @energy ci
+    wf = @energy casci
     @test isapprox(wf.energy,-75.0188434735,rtol=tol)
 end
