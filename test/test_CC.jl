26ac6f11817f81c8eecb0e51e03fc99c0cb40cd2
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2020-09-03 14:47:44 -0400

    Added test for ecCC

--- test/test_CC.jl
+++ test/test_CC.jl
@@ -1,9 +1,14 @@
 @reset
 @testset "Coupled Cluster" begin
-    wf = @energy ccsd;
+    wf = @energy CCSD;
     @test isapprox(wf.CorrelationEnergy, -75.0187095714, rtol=tol)
-    wf = @energy ccsd(t);
+    wf = @energy CCSD(T);
     @test isapprox(wf.correction, -7.38086348153572e-5, rtol=tol)
     wf = @energy ecCCSD;
     @test isapprox(wf.CorrelationEnergy, -75.0187251416, rtol=tol)
+    wf = @energy ecCCSD(T);
+    @test isapprox(wf.CorrelationEnergy, -75.0187989952, rtol=tol)
+    @set ci_alg sparse
+    wf = @energy ecCCSD;
+    @test isapprox(wf.CorrelationEnergy, -75.0188434735, rtol=tol)
 end
