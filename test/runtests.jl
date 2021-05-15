e7c894872870c2ddfb93392ec0b1785e0d5551b7
Gustavo Aroeira <gustavo.aroeira@gmail.com>
2021-05-14 23:23:02 -0400

    Update runtests.jl
    Fixing the changes I made while debugging and forgot to roll back because im dumb

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,12 @@ molecules = ["water",   "ammonia",     "benzene", "ethanol", "formaldehyde", "gl
 basis =     ["cc-pvtz", "aug-cc-pvdz", "6-31g",   "cc-pvdz", "6-31g*",       "sto-3g",  "cc-pvtz", "3-21g",         "6-311+g",     "cc-pcvdz"]
 
 tol = 1E-8
-@testset "Fermi" verbose = true begin
+@testset "Fermi" begin
     include("test_options.jl")
     #include("test_output.jl")
     #include("test_RHF.jl")
-    #include("test_MP.jl")
+    include("test_MP.jl")
     #include("test_CCSD.jl")
-    #include("test_pT.jl")
+    include("test_pT.jl")
     @reset
 end
-
-include("coverage.jl")
