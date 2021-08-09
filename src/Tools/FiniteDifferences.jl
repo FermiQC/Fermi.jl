22159e36002cda7d79fa5444a7c1a6486a155c35
gustavojra <gustavo.aroeira@gmail.com>
2021-08-09 19:13:26 -0400

    Finite differences grad working (rudimentar)

--- src/Tools/FiniteDifferences.jl
+++ src/Tools/FiniteDifferences.jl
@@ -2,14 +2,39 @@ function create_displacement(mol, A::Int, i::Int, h)
 
     new_atoms = [mol.atoms...]
     atomA = mol.atoms[A]
-    new_xyz = [atomA.xyz...] .+ h
+    new_xyz = [atomA.xyz...]
+    new_xyz[i] += h #* √(mass[atomA.Z]) * PhysicalConstants.bohr_to_angstrom
     new_atoms[A] = Fermi.Geometry.Atom(atomA.AtomicSymbol, atomA.Z, (new_xyz[1], new_xyz[2], new_xyz[3]))
 
     return Fermi.Geometry.Molecule(new_atoms, mol.charge, mol.multiplicity)
 end
 
-function gradient_test(energy_function, h=0.005)
-    mol = Fermi.Geometry.Molecule()
+function apply_gradient(mol, g, d = 0.001)
+    Zvals = [A.Z for A = mol.atoms]
+    Svals = [A.AtomicSymbol for A = mol.atoms]
+
+    new_atoms = Fermi.Geometry.Atom[]
+
+    for i = eachindex(Zvals)
+        x = mol.atoms[i].xyz[1] - d*g[i, 1]
+        y = mol.atoms[i].xyz[2] - d*g[i, 2]
+        z = mol.atoms[i].xyz[3] - d*g[i, 3]
+        push!(new_atoms, Fermi.Geometry.Atom(Svals[i], Zvals[i], (x,y,z)))
+    end
+
+    return Fermi.Geometry.Molecule(new_atoms, mol.charge, mol.multiplicity)
+end
+
+function geom_rms(mol1, mol2)
+    out = 0.0
+    N = length(mol1.atoms)
+    for a in 1:N
+        out += sum((mol1.atoms[a].xyz .- mol2.atoms[a].xyz).^2)
+    end
+    return √(out / 3*N)
+end
+
+function gradient_test(mol, energy_function, h=0.005)
     N = length(mol.atoms)
     Eplus = zeros(N,3)
     Eminus = zeros(N,3)
@@ -27,9 +52,41 @@ function gradient_test(energy_function, h=0.005)
         Eminus[A,i] = wfn.energy
     end
 
-    display(Eplus)
-    display(Eminus)
     g = (Eplus - Eminus) ./ (2*h)
 
     return g
+end
+
+function opt_test(energy_function; h=0.005, d=0.01)
+    @set printstyle none
+
+    scf_Etol  = Options.get("scf_e_conv")
+    scf_Dtol  = Options.get("scf_max_rms")
+    old_mol = Fermi.Geometry.Molecule()
+
+    # Central
+    wfn = eval(Expr(:call, energy_function, old_mol))
+    oldE = wfn.energy
+    println(format("Initial Energy: {:15.10f}", oldE))
+
+    ite = 1
+    dE = 1
+    while abs(dE) > 1e-8
+        g = gradient_test(old_mol, energy_function, h)
+        new_mol = apply_gradient(old_mol, g, d)
+        wfn = eval(Expr(:call, energy_function, new_mol))
+        if abs(wfn.e_conv) > scf_Etol || abs(wfn.d_conv) > scf_Dtol
+            display(new_mol)
+            error("SCF not converged")
+        end
+        dE = oldE - wfn.energy
+        RMS = √(sum(g.^2) / length(g))
+        GRMS = geom_rms(old_mol, new_mol)
+        println(format("Iter {:3}   Energy: {:15.10f}   ΔE: {:15.10f}   RMS: {:15.10f}   GRMS: {:15.10f}", ite, wfn.energy, dE, RMS, GRMS))
+        oldE = wfn.energy
+        old_mol = new_mol
+        ite += 1
+    end
+
+    display(old_mol)
 end
