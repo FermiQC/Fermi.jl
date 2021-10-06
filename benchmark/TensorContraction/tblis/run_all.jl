4eae92e90ebf5a99f445ab8dbe791b58c5b702a8
Gustavo <gustavo.aroeira@gmail.com>
2021-10-06 14:30:44 -0400

    Tensor Contraction benchmark done

---
+++ benchmark/TensorContraction/tblis/run_all.jl
@@ -0,0 +1,10 @@
+for N = 1:15
+    run(`julia --threads $N run_X_threads.jl`)
+    run(`mkdir N$N`)
+
+    for f in readdir(@__DIR__)
+        if occursin(".out", f)
+            mv(f, "N$N/$f")
+        end
+    end
+end
